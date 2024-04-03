6bc78025507eaf7471f6b62f4575422c94de8310
Shendy <Shendy1567@gmail.com>
2024-04-04 01:46:25 +0700

    add update and delete user

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const { Op } = require("sequelize");
 const { User } = require("../models");
+const { hashPassword } = require("../utils/passwordHandler");
 
 class UserControllers {
     async getUsers(req, res) {
@@ -61,7 +62,86 @@ class UserControllers {
         }
     }
 
-    async updateUser(req, res) {}
+    async updateUser(req, res) {
+        try {
+            const { id } = req.params;
+            const { name, email, password } = req.body;
+            const data = await User.findByPk(id);
+            if (!data)
+                return res.status(404).json({
+                    code: 404,
+                    message: `Player with id: ${id} not found`,
+                    data: null,
+                });
+            console.log(req.id);
+            if (id != req.id)
+                return res.status(401).json({
+                    code: 401,
+                    message: `Unauthorized user detected`,
+                    data: null,
+                });
+
+            const updateFields = {};
+
+            if (name) {
+                updateFields.name = name;
+            }
+
+            if (email) {
+                updateFields.email = email;
+            }
+
+            if (password) {
+                updateFields.password = await hashPassword(password);
+            }
+            const [updateCount] = await User.update(updateFields, {
+                where: { id: id },
+            });
+            if (updateCount === 1) {
+                return res.status(200).json({
+                    code: 401,
+                    message: `Player with id: ${id} successfully updated`,
+                    data: updateFields,
+                });
+            }
+        } catch (error) {
+            return res.status(500).json({
+                code: 500,
+                message: error.message,
+                data: null,
+            });
+        }
+    }
+    async deleteUser(req, res) {
+        try {
+            const { id } = req.params;
+            const user = await User.findByPk(id);
+            if (!user) {
+                return res.status(404).json({
+                    code: 404,
+                    message: `User with id ${id} not found`,
+                    data: null,
+                });
+            }
+
+            await User.destroy({
+                where: {
+                    id,
+                },
+            });
+            return res.status(200).json({
+                code: 200,
+                message: "User Successfully deleted",
+                data: user,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                code: 500,
+                message: error.message,
+                data: null,
+            });
+        }
+    }
 }
 
 module.exports = new UserControllers();
--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,7 +1,13 @@
 const userControllers = require("../../controller/userController");
 const router = require("express").Router();
+const verifyRoles = require("../../middleware/verifyRoles");
+const verifyJWT = require("../../middleware/verifyJWT");
 
-router.get("/", userControllers.getUsers);
 router.get("/:id", userControllers.getUserById);
+router.get("/", userControllers.getUsers);
+
+router.use(verifyJWT);
+router.put("/:id", userControllers.updateUser);
+router.delete("/:id", verifyRoles("admin"), userControllers.deleteUser);
 
 module.exports = router;
