71d8c74f97fad52488673065813817aed6c97646
Shendy <Shendy1567@gmail.com>
2024-04-04 01:05:01 +0700

    changing name from authCheck to verifyJWT

---
+++ middleware/verifyJWT.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer "))
+        return res.sendStatus(401);
+    const authToken = authHeader && authHeader.split(" ")[1];
+    if (!authToken) return res.sendStatus(401);
+    jwt.verify(authToken, process.env.TOKEN, (err, decoded) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        req.email = decoded.userEmail;
+        req.role = decoded.userRole;
+        next();
+    });
+};
+
+module.exports = verifyJWT;
